09b55732691f283bfa20b888ab149f1d354486d9
Andrew Pomerleau <arpomerleau@gmail.com>
2022-07-11 20:57:52 -0400

    first draft

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Item } from '../item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css'],
+})
+export class ItemComponent {
+  editable: boolean = false;
+
+  @Input()
+  item!: Item;
+  @Input()
+  newItem!: string;
+  @Output() remove = new EventEmitter<Item>();
+
+  saveItem(description: string) {
+    if (!description) return;
+
+    this.editable = false;
+    this.item.description = description;
+  }
+}
